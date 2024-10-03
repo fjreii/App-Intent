8232cfb8f0d62c54056a958e96ccd24cef5f9745
Fajri Nurhadi <144699516+fjreii@users.noreply.github.com>
2024-10-03 15:22:47 +0700

    Add files via upload

---
+++ src/main/java/com/mdproject/myintentapp/MoveActivity.kt
@@ -0,0 +1,9 @@
+package com.mdproject.myintentapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MoveActivity : AppCompatActivity()
